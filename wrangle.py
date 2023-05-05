91e37676c278ea0732689d0e72ef0f8db33adc09
Tyler Kephart <tkephart96@gmail.com>
2023-05-05 11:44:06 -0500

    combined functions to one

--- wrangle.py
+++ wrangle.py
@@ -11,24 +11,24 @@ from env import user,password,host
 
 ### ACQUIRE DATA ###
 
-def get_zillow(user=user,password=password,host=host):
+def wrangle_zillow(user=user,password=password,host=host):
     """
-    This function retrieves data from a MySQL database containing information about single family
-    residential properties and saves it to a CSV file or returns it as a pandas dataframe.
+    This function wrangles data from a cached CSV or SQL database, drops unneeded columns and null
+    values, and returns a cleaned dataframe of Zillow property data.
     
-    :param user: The username for the MySQL database connection
-    :param password: The password is unique per person pulled from personal env
+    :param user: The username for accessing the MySQL database
+    :param password: Please make sure to keep your password secure and not share it with anyone
     :param host: The host parameter is the address of the server where the MySQL database is hosted
-    :return: a pandas DataFrame containing information about single family residential properties in
-    2017 from the Zillow database. If the data has already been saved to a CSV file, the function reads
-    the data from the file. Otherwise, it reads the data from the database, saves it to a CSV file, and
-    returns the DataFrame.
+    :return: a pandas DataFrame containing cleaned and wrangled data from the Zillow database for single
+    family residential properties. The DataFrame includes columns for bedroom count, bathroom count,
+    calculated finished square footage, tax value in dollars, year built, tax amount, and FIPS code. The
+    function drops unneeded columns and null values before returning the DataFrame.
     """
     # name of cached csv
     filename = 'zillow2017.csv'
     # wrangle from cached data
     if os.path.isfile(filename):
-        return pd.read_csv(filename)
+        df = pd.read_csv(filename)
     # wrangle from sql db if not cached
     else:
         # read sql query into df
@@ -48,16 +48,6 @@ def get_zillow(user=user,password=password,host=host):
                             , f'mysql+pymysql://{user}:{password}@{host}/zillow')
         # cache data locally
         df.to_csv(filename, index=False)
-        return df
-
-def wrangle_zillow():
-    """
-    The function drops null values from a Zillow dataset and returns the cleaned dataframe.
-    :return: The function `wrangle_zillow()` is returning a cleaned and preprocessed dataframe obtained
-    from the `get_zillow()` function. The null values in the dataframe have been dropped as they account
-    for less than 1% of the data.
-    """
-    df = get_zillow()
     # drop unneeded columns
     df = df.drop(columns=['propertylandusetypeid','propertylandusedesc'])
     # nulls account for less than 1% so dropping
