7b91f5e1bc10ace429e5ad0e278913697924a0ae
Tyler Kephart <tkephart96@gmail.com>
2023-05-09 16:28:13 -0500

    added other scaling funcs

--- README.md
+++ README.md
@@ -14,24 +14,29 @@ Mapped FIPS to county names
 
 Handled outliers by filtering area to less than 25,000 and tax_value to less than its 95th quantile
 
+Dropped properties where beds or baths were 0, accounted for less than 1%
+
 Data after this is now about 2.03 mil rows
+
+I plan to scale year, beds, baths, area, and prop_tax using RobustScaler for now, may switch later
+
+Encoded county to 1s and 0s
 
 ## Data Dictionary
 
-| Original                     | Feature   | Type    | Values              | Definition                                               |
-| :--------------------------- | :-------- | :------ | :------------------ | :------------------------------------------------------- |
-| yearbuilt                    | year      | Year    | 1801-2016           | The year the principal residence was built               |
-| bedroomcnt                   | beds      | Numeric | 0-25                | Number of bedrooms in home                               |
-| bathroomcnt                  | baths     | Numeric | 0-32                | Number of bathrooms in home including fractional         |
-| calculatedfinishedsquarefeet | area      | SqFt    | 1~1mil              | Calculated total finished living area                    |
-| taxvaluedollarcnt            | tax_value | USD     | 1~98mil             | The total tax assessed value of the parcel/home          |
-| taxamount                    | prop_tax  | USD     | 1.85~1.3mil         | The total property tax assessed for that assessment year |
-| fips                         | county    | County  | LA, Orange, Ventura | Federal Information Processing Standard (these 3 in CA)  |
+| Original                     | Feature    | Type    | Values              | Definition                                               |
+| :--------------------------- | :--------- | :------ | :------------------ | :------------------------------------------------------- |
+| yearbuilt                    | year       | Year    | 1801-2016           | The year the principal residence was built               |
+| bedroomcnt                   | beds       | Numeric | 1-25                | Number of bedrooms in home                               |
+| bathroomcnt                  | baths      | Numeric | 0.5-32              | Number of bathrooms in home including fractional         |
+| calculatedfinishedsquarefeet | area       | SqFt    | 1~1mil              | Calculated total finished living area                    |
+| taxvaluedollarcnt (target)   | prop_value | USD     | 1~98mil             | The total tax assessed value of the parcel/home          |
+| taxamount                    | prop_tax   | USD     | 1.85~1.3mil         | The total property tax assessed for that assessment year |
+| fips                         | county     | County  | LA, Orange, Ventura | Federal Information Processing Standard (these 3 in CA)  |
+| Additional Features          |            | Numeric | 1=True, 0=False     | Encoded categorical variables                            |
 
 FIPS County Codes:
 
 * 06037 = LA County, CA
 * 06059 = Orange County, CA
 * 06111 = Ventura County, CA
-
-Might turn FIPS to string for encoding later since it is not continuous, it is more of a category
--- prepare.py
+++ prepare.py
@@ -1,4 +1,15 @@
-'''Wrangle Zillow Data'''
+'''
+Prepare Zillow Data
+
+Scalers:
+- mm_zillow
+- std_zillow
+- robs_zillow
+- qtf_zillow
+
+Encoder:
+- encode_county
+'''
 
 ### IMPORTS ###
 
@@ -7,6 +18,52 @@ import pandas as pd
 import sklearn.preprocessing
 
 ### SCALERS ###
+
+def mm_zillow(train,validate,test):
+    """
+    The function applies the Min Max Scaler method to scale the numerical features of the train, validate,
+    and test datasets.
+    
+    :param train: a pandas DataFrame containing the training data
+    :param validate: The validation dataset, which is used to evaluate the performance of the model
+    during training and to tune hyperparameters
+    :param test: The "test" parameter is a dataset that is used to evaluate the performance of a machine
+    learning model that has been trained on the "train" dataset and validated on the "validate" dataset.
+    The "test" dataset is typically used to simulate real-world scenarios and to ensure that the model
+    is able
+    :return: three dataframes: Xtr (scaled training data), Xv (scaled validation data), and Xt (scaled
+    test data).
+    """
+    mm_scale = sklearn.preprocessing.MinMaxScaler()
+    Xtr,Xv,Xt = train,validate,test
+    scale = ['year','beds','baths','area','prop_tax']
+    Xtr[scale] = mm_scale.fit_transform(Xtr[scale])
+    Xv[scale] = mm_scale.transform(Xv[scale])
+    Xt[scale] = mm_scale.transform(Xt[scale])
+    return Xtr, Xv, Xt
+
+def std_zillow(train,validate,test):
+    """
+    The function applies the Standard Scaler method to scale the numerical features of the train, validate,
+    and test datasets.
+    
+    :param train: a pandas DataFrame containing the training data
+    :param validate: The validation dataset, which is used to evaluate the performance of the model
+    during training and to tune hyperparameters
+    :param test: The "test" parameter is a dataset that is used to evaluate the performance of a machine
+    learning model that has been trained on the "train" dataset and validated on the "validate" dataset.
+    The "test" dataset is typically used to simulate real-world scenarios and to ensure that the model
+    is able
+    :return: three dataframes: Xtr (scaled training data), Xv (scaled validation data), and Xt (scaled
+    test data).
+    """
+    std_scale = sklearn.preprocessing.StandardScaler()
+    Xtr,Xv,Xt = train,validate,test
+    scale = ['year','beds','baths','area','prop_tax']
+    Xtr[scale] = std_scale.fit_transform(Xtr[scale])
+    Xv[scale] = std_scale.transform(Xv[scale])
+    Xt[scale] = std_scale.transform(Xt[scale])
+    return Xtr, Xv, Xt
 
 def robs_zillow(train,validate,test):
     """
@@ -25,7 +82,7 @@ def robs_zillow(train,validate,test):
     """
     rob_scale = sklearn.preprocessing.RobustScaler()
     Xtr,Xv,Xt = train,validate,test
-    scale = train.columns[:-1].to_list()
+    scale = ['year','beds','baths','area','prop_tax']
     Xtr[scale] = rob_scale.fit_transform(Xtr[scale])
     Xv[scale] = rob_scale.transform(Xv[scale])
     Xt[scale] = rob_scale.transform(Xt[scale])
@@ -50,8 +107,22 @@ def qtf_zillow(train,validate,test,out_dist='normal'):
     """
     qt = sklearn.preprocessing.QuantileTransformer(output_distribution=out_dist)
     Xtr,Xv,Xt = train,validate,test
-    scale = train.columns[:-1].to_list()
+    scale = ['year','beds','baths','area','prop_tax']
     Xtr[scale] = qt.fit_transform(Xtr[scale])
     Xv[scale] = qt.transform(Xv[scale])
     Xt[scale] = qt.transform(Xt[scale])
     return Xtr, Xv, Xt
+
+### ENCODE ###
+
+def encode_county(df):
+    '''
+    Encode county column from zillow dataset
+    '''
+    df['Orange'] = df.county.map({'Orange':1,'Ventura':0,'LA':0})
+    df['Ventura'] = df.county.map({'Orange':0,'Ventura':1,'LA':0})
+    return df
+
+
+
+
